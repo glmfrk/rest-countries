4699f56af06d40999a008163438c4cc4821f369b
Gulam Faruk <golamfaruk204@gmail.com>
2021-02-28 13:29:26 +0600

    Country Added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Country from './components/Countrys/Country';
 
+
 function App() {
   
   const [countries, setCountries] = useState([]); 
+  const [selectedCountry, setSelectedCountry] = useState([]);
+
   useEffect(()=>{
     fetch('https://restcountries.eu/rest/v2/all')
     .then(res => res.json())
@@ -12,11 +15,19 @@ function App() {
     .catch(error => console.log(error))
   }, [])
 
+  const handleAddCountry = (country)=> {
+    const newSelectCountry = [...selectedCountry, country];
+    setSelectedCountry(newSelectCountry);
+  }
+
+
   return (
     <div className="App">
       <h1>All Countries : {countries.length}</h1>
+      <h3>Select Countries Added : {selectedCountry.length}</h3>
+
         {
-          countries.map(country => <Country key={country.alpha3Code} country = {country}></Country>)
+          countries.map(country => <Country handleAddCountry = {handleAddCountry} key={country.alpha3Code} country = {country}></Country>)
         }
     </div>
   );
--- src/components/Countrys/Country.js
+++ src/components/Countrys/Country.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import "./Country.css"
 
 const Country = (props) => {
     const {name, region,population,flag} = props.country;
-    console.log(props.country)
+    // console.log(props.country)
+    const handleAddCountry = props.handleAddCountry;
     return (
-        <div>
+        <div className="content-wrapper">
            <h2>This is country : {name} </h2>
            <h4>Population : {population}</h4>
            <small>Region : {region}</small> 
            <p> flags : <img height="100" width="200" src={flag} alt="country flags" srcSet=""/> </p>
+           <button onClick={() => handleAddCountry(props.country)}>Add Country</button>
         </div>
     );
 };
